e233f80519feb83ebe453f56febdc1ec8d64409a
ekaterina-shavinceva <shavinceva-e@mail.ru>
2024-08-08 22:18:13 +0500

    Update views.py

--- news/views.py
+++ news/views.py
@@ -1,8 +1,10 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView)
 from .models import Post
 from .filters import PostFilter
 from datetime import datetime
-
+from .forms import NewsForm, ArticlesForm
+from .resources import news, article
+from django.urls import reverse_lazy
 
 class PostsList(ListView):
     model = Post
@@ -22,12 +24,78 @@ class PostsList(ListView):
         context['time_now'] = datetime.utcnow()
         return context
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['next_sale'] = None
-    #     return context
-
 class PostDetail(DetailView):
     model = Post
     template_name = 'post.html'
     context_object_name = 'post'
+
+
+class NewsSearch(ListView):
+    model = Post
+    ordering = '-post_time'
+    template_name = 'search.html'
+    context_object_name = 'news'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+class NewsPostList(ListView):
+    model = Post
+    ordring = '-date'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Post.objects.filter(content='NE')
+
+class ArticlePostList(ListView):
+    model = Post
+    ordring = '-date'
+    template_name = 'article_list.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Post.objects.filter(content='AR')
+
+class NewsCreate(CreateView):
+    permission_required = ('news.add_post',)
+    form_class = NewsForm
+    model = Post
+    template_name = 'post_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.content = news
+        return super().form_valid(form)
+
+class ArticlesCreate(CreateView):
+    permission_required = ('news.add_post',)
+    form_class = ArticlesForm
+    model = Post
+    template_name = 'post_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.content = article
+        return super().form_valid(form)
+
+class PostUpdate(UpdateView):
+    permission_required = ('news.change_post',)
+    form_class = NewsForm
+    model = Post
+    template_name = 'post_edit.html'
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
