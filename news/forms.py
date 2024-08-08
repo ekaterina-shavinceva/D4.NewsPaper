170950fc6dc0dc886ed4ba9704020791344df079
ekaterina-shavinceva <shavinceva-e@mail.ru>
2024-08-08 22:17:05 +0500

    Update forms.py

--- news/forms.py
+++ news/forms.py
@@ -3,13 +3,30 @@ from .models import Post
 from django.core.exceptions import ValidationError
 
 class NewsForm(forms.ModelForm):
-    title = forms.CharField(min_length = 1)
-    text = forms.CharField(min_length = 1)
+    title = forms.CharField(min_length=1)
+    text = forms.CharField(min_length=1)
     class Meta:
         model = Post
         fields = ['author', 'title', 'text']
 
     def clean(self):
+        cleaned_data = super().clean()
+        title = cleaned_data.get('title')
+        text = cleaned_data.get('text')
+        if title == text:
+            raise ValidationError({
+                'title':'Название не может совпадать с текстом'
+            })
+        return cleaned_data
+
+class ArticlesForm(forms.ModelForm):
+    title = forms.CharField(min_length=1)
+    text = forms.CharField(min_length=1)
+    class Meta:
+        model = Post
+        fields = ['author', 'title', 'text']
+
+    def clen(self):
         cleaned_data = super().clean()
         title = cleaned_data.get('title')
         text = cleaned_data.get('text')
