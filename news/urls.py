1ddd0f274ca2543f761eb85c6a3cab95248fcb7d
ekaterina-shavinceva <shavinceva-e@mail.ru>
2024-08-09 23:12:57 +0500

    Update urls.py

--- news/urls.py
+++ news/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
    path('search/', NewsSearch.as_view(), name='post_search'),
    path('create/', NewsCreate.as_view(), name='news_create'),
-   path('articles/create/', NewsCreate.as_view(), name='articles_create'),
+   path('articles/create/', NewsCreate.as_view(), name='news/articles_create'),
    path('<int:pk>/update/', PostUpdate.as_view(), name='post_update'),
    path('<int:pk>/delete/', PostDelete.as_view(), name='post_delete')
 ]
