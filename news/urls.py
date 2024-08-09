fb63c1ce2e5f6b309489f900fbe2a8bc25da2988
ekaterina-shavinceva <shavinceva-e@mail.ru>
2024-08-09 15:31:38 +0500

    Update urls.py

--- news/urls.py
+++ news/urls.py
@@ -4,12 +4,12 @@ from .views import PostsList, PostDetail, NewsSearch, NewsPostList, ArticlePostL
 
 urlpatterns = [
    path('', PostsList.as_view(), name='post_list'),
-   path ('<int:pk>', PostDetail.as_view(), name='post_detail'),
+   path('<int:pk>', PostDetail.as_view(), name='post_detail'),
    path('news/', NewsPostList.as_view()),
    path('articles/', ArticlePostList.as_view()),
    path('search/', NewsSearch.as_view(), name='post_search'),
    path('create/', NewsCreate.as_view(), name='news_create'),
    path('articles/create/', ArticlesCreate.as_view(), name='articles_create'),
    path('<int:pk>/update/', PostUpdate.as_view(), name='post_update'),
-    path('<int:pk>/delete/', PostDelete.as_view(), name='post_delete')
+   path('<int:pk>/delete/', PostDelete.as_view(), name='post_delete')
 ]
